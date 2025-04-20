9fe72756da1323b4184f6351eb5c9147ce521cb3
vinicius <144129498+viniboy2@users.noreply.github.com>
2025-04-20 19:36:31 -0300

    updaTE

--- estudos para prova/script.js
+++ estudos para prova/script.js
@@ -1,3 +1,14 @@
 
+let a = window.prompt("Digite o valor de a");
+let b = window.prompt("Digite o valor de b");
 
+// Convert the strings to numbers
+a = Number(a);
+b = Number(b);
 
+function somar(a, b) {
+    return a + b;    
+}
+
+let resultado = somar(a, b);  // Call the function with the values
+console.log(resultado);      // Log the result
